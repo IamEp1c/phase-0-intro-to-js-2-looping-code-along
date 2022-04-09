b570ed5f7f970d4932ad60a8a77db07db835c026
Waqas Anjum <Qanjum96@gmail.com>
2022-04-09 17:23:57 -0400

    commit tree badd45efe32af1cc23e1c0b8ddc1b59b723965eb

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+let messages = []
+
+function writeCards(names,  suprise){
+    for (let i = 0; i < names.length; i++){
+         messages.push(`Thank you, ${names[i]}, for the wonderful ${suprise} gift!`)
+    }
+    return messages
+}
+// writeCards(messages, suprise)
+
+// for jon <3 
+
+function countDown(num){
+    while (num >= 0){
+        console.log(num--)
+    }
+}
+
